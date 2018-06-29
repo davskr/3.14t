1b37832cec143c2171d1d8aba9c619a0bd83836f
davskr <32640690+davskr@users.noreply.github.com>
2018-06-29 17:36:29 +0200

    V1
    first exercise

---
+++ ex3.py
@@ -0,0 +1,17 @@
+# =============================================================================
+# LISTS
+# =============================================================================
+
+#   l = [] is a vector
+l = ['david', 'alen', 'mama', 'tata', 'sin', 333, 'drake']
+
+print(l[0])
+
+#   the full code
+print(l[len(l)-1])
+
+#   the variable code
+zadnja = len(l)-4
+print(l[zadnja])
+
+
---
+++ read_me.txt
@@ -0,0 +1,7 @@
+import numpy as np
+
+ ==============================================================================
+ this file is non-actionable
+ because it is a .txt file
+ all Python executable files are titled with .py
+ ==============================================================================
